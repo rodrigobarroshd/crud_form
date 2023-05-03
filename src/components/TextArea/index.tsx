6fffe4a29a24669f8a186df4e8e89d3451920f5f
Rodrigo Barros <brodrigo87@gmail.com>
2023-05-03 09:43:39 -0300

    test

--- src/components/TextArea/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { TextInputProps } from 'react-native';
-
-import { Container } from './styles';
-
-export function TextArea({ ...rest }: TextInputProps) {
-  return <Container multiline {...rest} />
-}
